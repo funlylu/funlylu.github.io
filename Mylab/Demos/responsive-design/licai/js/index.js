43bddbb0bb51d01b3febe34ba3731fe18df0d871
funlylu <fanglylu@foxmail.com>
2017-07-31 01:23:27 +0800

    Site updated: 2017-07-31 01:23:25

--- Mylab/Demos/responsive-design/licai/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        autoplay: true,
-        loop: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: true
-    });
-});
